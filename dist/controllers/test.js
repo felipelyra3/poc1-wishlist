fe0c53d4597fc8d57b4b6bdae3ed95ea94c6698f
felipelyra3 <felipelyra3@gmail.com>
2022-11-09 19:34:19 -0300

    Ignore Dist Folder

--- dist/controllers/test.js
+++
@@ -1,6 +0,0 @@
-function Double(req, res) {
-    var double = Number(req.query.value) * 2;
-    res.send(double);
-    //res.send('ok');
-}
-export { Double };
--- dist/schemas/addUserSchema.js
+++
@@ -1,6 +0,0 @@
-var Joi = require("joi");
-var addUserSchema = Joi.object({
-    name: Joi.string().empty().max(255).required(),
-    email: Joi.string().empty().email().max(255).required()
-});
-export { addUserSchema };
